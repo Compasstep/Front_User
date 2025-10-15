57dc28a8e5e805f4a75ef67e14faab31769d2162
th-mong <taehyeong8580@gmail.com>
2025-10-15 15:33:06 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import "@fontsource/anton/400.css";
+import "@fontsource/quicksand/400.css";
+import "@fontsource/noto-sans-kr/400.css";
+import "@fontsource/black-han-sans/400.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
