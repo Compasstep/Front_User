0601eca16d3b5f7138d400f9e8bebcca5a752c1a
th-mong <taehyeong8580@gmail.com>
2025-11-19 01:09:27 +0900

    [Fix]s3오류수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,27 @@
 // src/components/Layout.jsx
+
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header.jsx';
 import SideNav from './SideNav.jsx';
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import useUserStore from '../store/userStore.js';
 
 function Layout() {
   const location = useLocation();
 
-  // 🔹 padding-top 제거가 필요한 페이지
+  const bootstrapAuth = useUserStore((s) => s.bootstrapAuth);
+
+  // 🔥 앱이 Layout을 로드할 때마다 user 최신화
+  useEffect(() => {
+    bootstrapAuth();
+  }, [bootstrapAuth]);
+
   const isSpecialLayout =
     location.pathname === '/' ||
     location.pathname === '/discovery/keyword' ||
     location.pathname === '/analysis/reputation';
 
-  // 🔹 로그인 페이지에서는 SideNav 숨김
   const hideSideNav = location.pathname === '/login';
 
   return (
